233b94a3c2857a784f39e1ce91f4f99626606893
zubinraj <mangachalil@gmail.com>
2013-01-13 19:32:12 -0600

    Initial commit

---
+++ js/zrgallery.js
@@ -0,0 +1,232 @@
+
+	var $container = jQuery('#gallery-container');
+
+
+    $container.imagesLoaded( function(){
+		$container.isotope({
+			itemSelector : '.gallery-item',
+			isAnimated : true
+			//isFitWidth : true,
+			//resizable: false // disable normal resizing
+			// set columnWidth to a percentage of container width
+			//masonry: { columnWidth: $container.width() / 3 }
+			
+		});
+		
+    });
+	
+
+	// more requests
+	jQuery("#more").click(function(event){
+		loadMore ();
+		event.preventDefault();
+	});
+
+
+	
+	var halfWay = (jQuery(document).height()/2);
+	var inAction = false;
+	var reachedEnd = false;
+	
+	jQuery(document).scroll(function(){
+	
+		halfWay = (jQuery(document).height()/2) - 500;
+		
+		if ( inAction == false ) {
+			if(jQuery(document).scrollTop() > halfWay){
+			
+				loadMore();
+			}
+		}		
+	}); 	
+
+	function loadMore()
+	{
+		inAction = true;
+		
+		var p = jQuery("#zrgallery_params").text();
+		//alert (p);
+		var data = {
+			action: 'zr_gallery_get',
+			param: p 
+		};
+		
+		jQuery.ajax({
+			type: "post",
+			url: "/photos/wp-admin/admin-ajax.php",
+			data: data,
+			//cache: false,
+			beforeSend: toggleLoadingImg(),		// show loading image
+			success: function(html){			// handle success
+				var success = processLoadMoreResponse (html);
+
+				// reset this flag so that the next scroll can pull in more items
+				//inAction = false;  // moved to the isotope function, as that is when it really completes
+			},
+			error: function (html) {
+				//alert ('No more items to load');
+				//toggleLoadMoreButton();
+			},
+			complete: function (html) {
+				toggleLoadingImg();
+			}
+
+		});
+	}
+
+	function processLoadMoreResponse(newElements) {
+
+		// remove the existing parameter element
+		$container.find("#zrgallery_params").remove();
+
+		//alert (newElements);
+		//hide new items while they are loading
+		var $newElems = jQuery( newElements ).css({ opacity: 0 });
+
+		// ensure that images load before adding to masonry layout
+		$newElems.imagesLoaded(function(){
+			// show elems now they're ready
+			$newElems.animate({ opacity: 1 });
+
+			$container.append( $newElems ).isotope( 'appended', $newElems );
+			
+			// reset this flag so that the next scroll can pull in more items
+			inAction = false;
+
+		}); 
+
+	}
+	
+	
+/*	function getMoreLink(html) {
+		// Get the more link from the html of the new page, else get from current page
+		if (html) {
+			return (jQuery(html).find("#more a").attr("href"));
+		}
+		else {
+			return (jQuery("#more a").attr("href"));
+		}
+	}
+
+
+	
+	function setMoreLink(newlink) {
+		jQuery("#more a").attr("href", newlink)
+	}
+*/	
+	function toggleLoadingImg() {
+		//alert ('toggle');
+		jQuery("#loading").toggle();
+	}
+
+	function toggleLoadMoreButton() {
+		jQuery("#more").toggle();
+	}
+
+
+/* start jquery image gallery */
+
+/* fade in the footer on hover */
+/*
+jQuery(function() {
+   jQuery('.ui-dialog-footer').css('display','none');
+   
+   jQuery('.ui-dialog-content').hover(function() {
+      alert ('in');
+      jQuery('.gallery-dialog-fullscreen .ui-dialog-footer',this).fadeIn();
+            
+      }, function() { jQuery('.gallery-dialog-fullscreen .ui-dialog-footer').fadeOut();
+   });
+});	
+*/
+
+jQuery(function () {
+    'use strict';
+
+    // Initialize the Image Gallery widget:
+    jQuery('#gallery-container').imagegallery();
+
+	
+	jQuery('#gallery-container').imagegallery('option', {
+		show: 'fade',
+		hide: 'fade',
+		fullscreen: true,
+		canvas: false,
+		modal: true
+	});
+	
+	
+/*	
+    // Initialize the theme switcher:
+    jQuery('#theme-switcher').change(function () {
+        var theme = jQuery('#theme');
+        theme.prop(
+            'href',
+            theme.prop('href').replace(
+                /[\w\-]+\/jquery-ui.css/,
+                jQuery(this).val() + '/jquery-ui.css'
+            )
+        );
+    });
+
+    // Create a buttonset out of the checkbox options:
+    jQuery('#buttonset').buttonset();
+
+    // Listen to options changes:
+    jQuery('#buttonset input, #effect').change(function () {
+        jQuery('#gallery').imagegallery('option', {
+            show: jQuery('#effect').val(),
+            hide: jQuery('#effect').val(),
+            fullscreen: jQuery('#option-fullscreen').is(':checked'),
+            slideshow: jQuery('#option-slideshow').is(':checked') && 5000
+        });
+    });
+
+    // Enable real fullscreen mode:
+    jQuery('#option-fullscreen').click(function () {
+        var checkbox = jQuery(this),
+            root = document.documentElement;
+        if (checkbox.is(':checked')) {
+            if (root.webkitRequestFullScreen) {
+                root.webkitRequestFullScreen(
+                    window.Element.ALLOW_KEYBOARD_INPUT
+                );
+            } else if (root.mozRequestFullScreen) {
+                root.mozRequestFullScreen();
+            }
+        } else {
+            (document.webkitCancelFullScreen ||
+                document.mozCancelFullScreen ||
+                jQuery.noop).apply(document);
+        }
+    });
+
+    // Load images via flickr for demonstration purposes:
+    jQuery.ajax({
+        url: 'http://api.flickr.com/services/rest/',
+        data: {
+            format: 'json',
+            method: 'flickr.interestingness.getList',
+            api_key: '7617adae70159d09ba78cfec73c13be3'
+        },
+	    dataType: 'jsonp',
+        jsonp: 'jsoncallback'
+    }).done(function (data) {
+        var gallery = jQuery('#gallery'),
+            url;
+        jQuery.each(data.photos.photo, function (index, photo) {
+            url = 'http://farm' + photo.farm + '.static.flickr.com/' +
+                photo.server + '/' + photo.id + '_' + photo.secret;
+            jQuery('<a rel="gallery"/>')
+                .append(jQuery('<img>').prop('src', url + '_s.jpg'))
+                .prop('href', url + '_b.jpg')
+                .prop('title', photo.title)
+                .appendTo(gallery);
+        });
+    });
+*/
+
+/* end jquery image gallery */
+
+
+});
---
+++ zrgallery.php
@@ -0,0 +1,329 @@
+<?php
+   /*
+   Plugin Name: ZR Gallery
+   Plugin URI: 
+   Description: Show your wordpress gallery images in a portfolio format. Uses the wonderful jquery masonry by Desandro
+   Version: 0.1
+   Author: Zubin Raj
+   Author URI: http://www.zubinraj.com
+   License: GPL2
+   */
+?>
+<?php
+
+//ZRGallery_Shortcode::init();
+
+add_action('init', 'ZRGallery_Shortcode::init');
+
+add_action('wp_ajax_zr_gallery_get', 'zr_gallery_get_callback');
+add_action('wp_ajax_nopriv_zr_gallery_get', 'zr_gallery_get_callback');
+
+
+function zr_gallery_get_callback() {
+
+	$param = stripslashes ($_POST['param']) ;
+	
+	echo ZRGallery_Shortcode::get_gallery_items($param);
+
+	die(); // this is required to return a proper result
+}
+
+class ZRGallery_Shortcode {
+	static $add_script;
+	
+
+	static function init() {
+
+		//add_action('init', array(__CLASS__, 'ZRGallery_Shortcode::register_script'));
+		
+		// register the scripts and add an action to print if necessary
+		ZRGallery_Shortcode::register_script();
+		add_action('wp_footer', array(__CLASS__, 'ZRGallery_Shortcode::print_script'));
+
+		add_shortcode('zrgallery', array(__CLASS__, 'zrgallery_shortcode_handler'));
+
+	}
+
+	// [zrgallery items_per_page="-1" category=""]
+	static function zrgallery_shortcode_handler( $atts ) {
+		
+		// determines if the script has to be loaded
+		self::$add_script = true;
+
+		extract( shortcode_atts( array(
+			'count' => '-1',
+			'category' => '',
+			'tag' => ''
+		), $atts ) );
+
+		$param = json_encode(array('category' => $category, 'tag' => $tag, 'count' => $count, 'offset' => 0));
+
+		return ZRGallery_Shortcode::display_gallery( $param );
+	}
+	
+	static function register_script() {
+
+		// Register scripts
+		wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js' );
+		wp_register_script('jquery-isotope', plugins_url( '/js/jquery.isotope.min.js', __FILE__ ));
+		wp_register_script('jquery-ui', plugins_url( '/js/jquery-ui.min-1.8.23.js', __FILE__ ));
+		wp_register_script('jquery-image-gallery-load-image', plugins_url( '/js/load-image.min.js', __FILE__ ));
+		wp_register_script('jquery-image-gallery', plugins_url( '/js/jquery.image-gallery.js', __FILE__ ));
+		wp_register_script('zrgallery', plugins_url( '/js/zrgallery.js', __FILE__ ));
+		wp_register_script('zrgallery', plugins_url( '/js/zrgallery.js', __FILE__ ));
+
+		// Register styles
+		wp_register_style('zrgallery', plugins_url( '/css/zrgallery.css', __FILE__ ));
+		wp_register_style('jquery-ui', plugins_url( '/css/jquery-ui.css', __FILE__ ));
+		wp_register_style('jquery-image-gallery', plugins_url( '/css/jquery.image-gallery.css', __FILE__ ));
+
+	}
+
+	static function print_script() {
+
+		// skip loading the scripts if the shortcode is not used in the page
+		if ( ! self::$add_script )
+			return;
+
+		// Enqueue scripts
+		wp_enqueue_script('jquery');
+		wp_enqueue_script('jquery-isotope');
+		wp_enqueue_script('jquery-ui');
+		wp_enqueue_script('jquery-image-gallery-load-image');
+		wp_enqueue_script('jquery-image-gallery');
+		wp_enqueue_script('zrgallery');
+
+		// Enqueue styles
+		wp_enqueue_style('zrgallery');
+		wp_enqueue_style('jquery-ui');
+		wp_enqueue_style('jquery-image-gallery');
+
+	}
+	
+	static function display_gallery( $param ) {
+
+		$str = '';
+		$str .= '<div id="gallery-container">';
+
+		$str .= ZRGallery_Shortcode::get_gallery_items( $param );
+	
+		$str .= '</div>';
+		
+		$str .= '<div style="clear:both"></div>';
+		$str .= '<div id="loading"><img src="' . plugins_url('/images/loading.gif', __FILE__) . '" /></div>';
+	
+		return $str;
+
+	
+	}
+	
+	static function get_gallery_items($param)
+	{
+		//var_dump ($param);
+	
+		$p = json_decode($param, true);
+
+		if (!is_numeric($p['count']))
+			$p['count'] = 20; //default
+		
+		if (!is_numeric($p['offset']))
+			$p['offset'] = 0; //default
+		
+		
+		$posts = ZRGallery_Shortcode::get_gallery_items_array($p['category'], $p['tag'], $p['count'], $p['offset']); 
+
+		
+		if (count($posts) <= 0)
+		{
+			header('HTTP/1.0 404 Not Found');
+			echo "<h5>Sorry, there are no items to display.</h5>";
+			return '';
+		}
+		
+		$str = '';
+
+		// calculate the next offset
+		$nextOffset = 0;
+		if ($p['count'] == -1)
+		{
+			// just an imaginary high number, so that it doesn't try to load again
+			$p['count'] =  9999999;   
+			$p['offset'] =  9999999;   
+		}
+		else
+		{
+			$p['offset'] += $p['count'];
+		}
+		
+		// re-encode with the new params
+		$p = json_encode(array('category' => $p['category'], 'tag' => $p['tag'], 'count' => $p['count'], 'offset' => $p['offset']));
+		
+		$str .= '<span id="zrgallery_params">' . $p . '</span>';
+
+		foreach($posts as $post){
+			$str .= str_replace("'","'",$post);
+		}
+
+		return $str;
+	}
+
+	static function get_gallery_items_array( $category_id, $tag, $count, $offset )
+	{
+		$items = array();
+
+		$args = array ( 
+						'category'		=> $category_id,
+						'numberposts' 	=> $count, 
+						'offset'		=> $offset,
+						'post_status' 	=> 'publish'
+					); 
+						
+		$posts = get_posts( $args );
+		
+		if ($posts) 
+		{
+			foreach ( $posts as $post ) 
+			{
+				$permalink = get_permalink( $post->ID );
+				
+				$title = apply_filters( 'the_title' , $post->post_title );
+
+				$gallery_rel = 'gallery|' . $permalink;
+
+				// get the attachments
+				$attargs = array( 
+								'post_type' => 'attachment', 
+								'numberposts' => -1, 
+								'post_status' => null, 
+								'post_parent' => $post->ID 
+								); 
+				
+				$attachments = get_posts($attargs);
+				
+				if ($attachments) 
+				{
+					foreach ( $attachments as $attachment ) 
+					{
+						$image_thumb = wp_get_attachment_image_src( $attachment->ID, 'medium' );
+						$image_full = wp_get_attachment_image_src ( $attachment-> ID, 'original' );
+
+						// sanity checks
+						if (!$image_thumb) echo 'No thumb';
+						if (!$image_full) echo 'No full';
+						
+						//echo apply_filters( 'the_title' , $attachment->post_title );
+						//the_attachment_link( $attachment->ID , false );
+						
+						$item = '<div class="gallery-item">
+									<a href="' . $image_full[0] . '" title="' . $title . '" rel="' . $gallery_rel . '" >
+									<img title="' . $title . '" alt="' . $title . '" src="' . $image_thumb[0] . '" />
+									<div class="overlay" title="' . $title . '" href="javascript:void(0);">
+										<div class="content">
+											<h5>' . $title . '</h5>
+										</div>
+									</div>
+									</a>
+								</div>';		
+					
+						//echo $item;
+
+						//								<div>
+						//									<div class="social-button facebook"><iframe src="//www.facebook.com/plugins/like.php?href='. $parent_permalink .'&amp;send=false&amp;layout=button_count&amp;width=100&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=20" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:20px;"allowTransparency="true"></iframe></div>
+						//								</div>
+						
+						array_push($items, $item);
+					}
+				}
+					
+
+			}
+
+		}
+		
+		return $items;
+		
+	}
+	
+	/*
+	static function get_gallery_items_array($category, $tag, $count, $offset)
+	{
+		$args = array
+					( 'post_type' 		=> 'attachment', 
+						'numberposts' 	=> $count, 
+						'offset'		=> $offset, 
+						'post_status' 	=> null, 
+						'post_parent' 	=> null
+					); 
+						//'orderby'		=> 'post_date',
+						//'order'       	=> 'DESC'); 
+						
+		$attachments = get_posts( $args );
+		
+		$items = array();
+			
+		if ($attachments) 
+		{
+			foreach ( $attachments as $attachment ) 
+			{
+				// Skip images not attached to a post
+				if (!$attachment->post_parent) continue;
+				
+				$parent_id = $attachment->post_parent;
+
+				// Skip images in posts that are not published
+				if (get_post_status ( $parent_id ) != 'publish') continue;
+				
+				$parent_title = get_the_title( $parent_id );
+				$parent_permalink = get_permalink( $parent_id );
+				
+				$image_thumb = wp_get_attachment_image_src( $attachment->ID, 'medium' );
+				$image_full = wp_get_attachment_image_src ( $attachment-> ID, 'original' );
+
+				$gallery_rel = 'gallery|' . $parent_permalink;
+				
+				// Get categories
+				$post_categories = wp_get_post_categories( $parent_id );
+				$categories_str = '';
+
+				// Filter based on categories
+				if ($category != '') {
+					if (!in_array($category, $post_categories)) continue;
+				}
+				
+				foreach($post_categories as $c)
+				{
+					$cat = get_category( $c );
+					$categories_str .= $cat->slug;
+				}
+
+				$item = '<div class="gallery-item ' . $categories_str . '">
+							<a href="' . $image_full[0] . '" title="' . $parent_title . '" rel="' . $gallery_rel . '" >
+							<img title="' . $parent_title . '" alt="' . $parent_title . '" src="' . $image_thumb[0] . '" />
+							<div class="overlay" title="' . $parent_title . '" href="javascript:void(0);">
+								<div class="content">
+									<h5>' . $parent_title . '</h5>
+								</div>
+							</div>
+							</a>
+						</div>';		
+			
+				//echo $item;
+
+	//								<div>
+	//									<div class="social-button facebook"><iframe src="//www.facebook.com/plugins/like.php?href='. $parent_permalink .'&amp;send=false&amp;layout=button_count&amp;width=100&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=20" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:20px;"allowTransparency="true"></iframe></div>
+	//								</div>
+				
+				array_push($items, $item);
+
+			}
+
+		}
+		
+		return $items;
+		
+	}
+	*/
+	
+
+}
+?>
